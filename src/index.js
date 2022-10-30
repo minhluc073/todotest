e563807a75fcdaa32728d4a81d40b32c5c9a83ba
minhluc073 <nguyenminhluc0703@gmail.com>
2022-10-30 21:53:31 +0700

    commit movie

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { compose, createStore } from 'redux';
 import { rootReducer } from './redux/reducers/rootReducer';
 
+const composeSetup = process.env.NODE_ENV !== 'production' && typeof window === 'object' &&  
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const store = createStore(rootReducer);
+const store = createStore(rootReducer,composeSetup());
 root.render(
   <Provider store={store}>
     <App />
